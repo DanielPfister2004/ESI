ffc64adba4b6fba287da819338a0c33b33409c0e
DanielPfister2004 <daniel.pfister@edu.fh-joanneum.at>
2025-11-17 09:02:00 +0100

    added Lab6 plank file

---
+++ Laboruebung_6/Hardware HAL/hal_wdt.h
@@ -0,0 +1,13 @@
+/*
+ * hal_wdt.h
+ *
+ *  Created on: 30.09.2025
+ *      Author: danie
+ */
+
+#ifndef HARDWARE_HAL_HAL_WDT_H_
+#define HARDWARE_HAL_HAL_WDT_H_
+
+void hal_WdtInit();
+
+#endif /* HARDWARE_HAL_HAL_WDT_H_ */
